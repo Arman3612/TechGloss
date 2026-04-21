c061a66d05808a4997b00b8797ff8e1da1787981
Armankhan13 <arman.9c123@gmail.com>
2026-04-22 00:57:39 +0530

    Update backend server and frontend auth

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ dotenv.config();
 
 const app = express();
 app.use(cors({
-  origin: ['http://localhost:5500', 'http://127.0.0.1:5500'],
+  origin: ['http://localhost:5500', 'http://127.0.0.1:5500', 'https://tech-gloss-lilac.vercel.app'],
   credentials: true
 }));
 app.use(express.json());
